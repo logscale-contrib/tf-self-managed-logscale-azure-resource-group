e84154e1dea931198eee88009a36e24e6e66711d
Ryan Faircloth <ryan@dss-i.com>
2022-10-04 14:54:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 
-resource "random_id" "rg-name" {
-  byte_length = 6
+resource "random_pet" "rg-name" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name_prefix}-${random_id.rg-name.id}"
+  name     = "${var.resource_group_name_prefix}-${random_pet.rg-name.id}"
   location = var.resource_group_location
   tags     = var.tags
 }
