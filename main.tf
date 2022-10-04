ac98526db91c82ae9beca16707633aaefc200338
Ryan Faircloth <ryan@dss-i.com>
2022-10-04 16:22:11 -0400

    feat: use static name

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 
-resource "random_pet" "rg-name" {
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name_prefix}-${random_pet.rg-name.id}"
+  name     = var.resource_group_name
   location = var.resource_group_location
   tags     = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
-variable "resource_group_name_prefix" {
-  default     = "logscale"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+variable "resource_group_name" {
+  type = string
 }
 variable "resource_group_location" {
   description = "Location of the resource group."
